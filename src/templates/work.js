1fad0ae045dbb8fada86b92b37d0ba4a2b0a53d3
jessicaGithub <himechi90@gmail.com>
2020-10-12 10:03:40 +1100

    update slider to support zooming in

--- src/templates/work.js
+++ src/templates/work.js
@@ -4,6 +4,7 @@ import { HelmetDatoCms } from "gatsby-source-datocms";
 import Img from "gatsby-image";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import { ImageGroup, Image } from "react-fullscreen-image";
 
 export default ({ data }) => (
   <Layout>
@@ -12,18 +13,8 @@ export default ({ data }) => (
       <div className="sheet__inner">
         <h1 className="sheet__title">{data.datoCmsWork.title}</h1>
         <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
-        <div className="sheet__slider">
-          {data.datoCmsWork.gallery.length > 0 && (
-            <Slider infinite={true} slidesToShow={1} arrows>
-              {data.datoCmsWork.gallery.map(({ fluid }) => (
-                <img
-                  alt={data.datoCmsWork.title}
-                  key={fluid.src}
-                  src={fluid.src}
-                />
-              ))}
-            </Slider>
-          )}
+        <div className="sheet__gallery">
+          <Img fluid={data.datoCmsWork.coverImage.fluid} />
         </div>
         <div
           className="sheet__body"
@@ -31,8 +22,31 @@ export default ({ data }) => (
             __html: data.datoCmsWork.descriptionNode.childMarkdownRemark.html
           }}
         />
-        <div className="sheet__gallery">
-          <Img fluid={data.datoCmsWork.coverImage.fluid} />
+        <div className="sheet__slider">
+          {data.datoCmsWork.gallery.length > 0 && (
+            <ImageGroup>
+              <ul className="images">
+                {data.datoCmsWork.gallery.map(i => (
+                  <li key={i}>
+                    <Image
+                      src={i.fluid.src}
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        display: "block",
+                        height: "100%",
+                        width: "100%",
+                        objectFit: "contain"
+                      }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </ImageGroup>
+          )}
         </div>
       </div>
     </article>
@@ -48,7 +62,7 @@ export const query = graphql`
       title
       excerpt
       gallery {
-        fluid(maxWidth: 200, imgixParams: { fm: "jpg", auto: "compress" }) {
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
           src
         }
       }
